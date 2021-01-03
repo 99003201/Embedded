801f27da9787711baff77059d5525a93aa0da760
Androthu Murali Satya Pavan <75420385+99003201@users.noreply.github.com>
2021-01-03 22:02:56 +0530

    Add files via upload

---
+++ Activities/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int c=0,d=0;
+	int result=0;
+	printf("Enter two numbers to perform subtraction");
+	scanf("%d",&c);
+	scanf("%d",&d);
+	result=c-d;
+	printf("The Subtraction for result is %d",result);
+
+}
